434476739679d12c3bc5126d65aa6d691f35f3de
mrKazzila <mrkazzila@gmail.com>
2024-03-06 21:08:07 +0300

    feat: add schemas

---
+++ backend/app/schemas/tokens.py
@@ -0,0 +1,34 @@
+from enum import StrEnum
+
+from pydantic import BaseModel, EmailStr
+
+__all__ = ["SAccessToken", "SRefreshToken", "STokens", "STokenTypes", "STokenData"]
+
+
+class STokenBase(BaseModel):
+    token_type: str = "bearer"
+
+
+class SAccessToken(STokenBase):
+    access_token: str
+
+
+class SRefreshToken(STokenBase):
+    refresh_token: str
+
+
+class STokens(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class STokenTypes(StrEnum):
+    access = "access"
+    refresh = "refresh"
+
+
+class STokenData(BaseModel):
+    email: EmailStr
+    type: STokenTypes
+    expiration: int
