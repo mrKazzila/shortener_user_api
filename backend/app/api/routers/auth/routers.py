8ae03f57eadce874abbda9ece911562866a1efd6
mrKazzila <mrkazzila@gmail.com>
2025-04-15 21:19:59 +0300

    refactor: remove cookies

--- backend/app/api/routers/auth/routers.py
+++ backend/app/api/routers/auth/routers.py
@@ -2,11 +2,10 @@ import logging
 from typing import Annotated
 
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.api.routers.auth._types import QueryRefreshToken
-from app.api.schemas.tokens import STokens
+from app.api.schemas.tokens import SRefreshTokenRequest, STokens
 from app.dto.users import UserFormDataDTO
 from app.service_layer.services import UsersServices
 from app.utils import TokenManager
@@ -24,7 +23,6 @@ router = APIRouter(
 
 @router.post("/login")
 async def login_user(
-    response: Response,
     form_user_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     user_service: FromDishka[UsersServices],
     token_manager: FromDishka[TokenManager],
@@ -40,11 +38,6 @@ async def login_user(
         email=form_user_data.username,
     )
 
-    token_manager.set_token_to_cookie(
-        response=response,
-        refresh_token=token_pair.refresh_token,
-    )
-
     return STokens(
         access_token=token_pair.access_token,
         refresh_token=token_pair.refresh_token,
@@ -56,19 +49,13 @@ async def login_user(
     summary="Refresh token",
 )
 def token_refresh(
-    response: Response,
     token_manager: FromDishka[TokenManager],
-    token: QueryRefreshToken,
+    token: SRefreshTokenRequest,
 ) -> STokens:
-    token_data = token_manager.verify_refresh_token(token=token)
+    token_data = token_manager.verify_refresh_token(token=token.token)
 
     new_token_pair = token_manager.update_token_pair(
         email=token_data.email,
-    )
-
-    token_manager.set_token_to_cookie(
-        response=response,
-        refresh_token=new_token_pair.refresh_token,
     )
 
     return STokens(
